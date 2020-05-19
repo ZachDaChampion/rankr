fadebf21b648bfc9ab4bd9fc1d233e2462dca503
ZachDaChampion <zachchampion79@gmail.com>
2020-05-18 22:09:36 -0700

    Add and animate search bar

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="search-box">
+    <input
+      id="box"
+      v-model="msg"
+      placeholder="Search for a show to rank"
+      :style="{ width: msg.length > 14 ? 'calc(100% - 256px)' : '420px' }"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class SearchBox extends Vue {
+  msg = "";
+}
+</script>
+
+<style scoped>
+#box {
+  padding: 16px 24px 16px 24px;
+  border: none;
+  font-size: 32px;
+  text-align: center;
+  color: #282a36;
+  -webkit-border-radius: 5px;
+  border-radius: 100vh;
+  outline: none;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  transform: translateY(50%);
+  transition: 250ms, width 500ms;
+}
+
+#box:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+
+#box:focus {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+</style>
--- src/components/TitleArea.vue
+++ src/components/TitleArea.vue
@@ -2,20 +2,23 @@
   <div class="title-area">
     <h1>rankr</h1>
     <h2>Rank things accurately.</h2>
+    <SearchBox />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import SearchBox from "@/components/SearchBox.vue";
 
-@Component
+@Component({
+  components: { SearchBox }
+})
 export default class TitleArea extends Vue {}
 </script>
 
 <style scoped>
 .title-area {
   background-color: #383a59;
-  padding: 32px;
 }
 
 h1 {
