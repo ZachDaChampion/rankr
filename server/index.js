a4db0f9210cb9e6e9b90d10bc3a949d1263ee63d
ZachDaChampion <zachchampion79@gmail.com>
2020-05-30 18:21:32 -0400

    Send missing poster image from server
    It would have previously been inserted by the client, which doesn't make much sense since the server is meant to handle all direct interaction with the themoviedb api.

--- server/index.js
+++ server/index.js
@@ -2,22 +2,51 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const axios = require("axios");
+const url = require("url");
 require("dotenv").config();
 
 app.get("/", (req, res) => res.send("hello world"));
 
-app.get("/tvsearch", (req, res) => {
+app.get("/placeholderposter", (req, res) => {
+  console.log("file");
+  return res.sendFile("no_poster.jpg", {
+    root: __dirname,
+    dotfiles: "deny",
+    headers: {
+      "x-timestamp": Date.now(),
+      "x-sent": true,
+    },
+  });
+});
+
+app.get("/tvsearch", async (req, res) => {
   console.log("TV SEARCH", req.query.search);
-  axios
-    .get(`https://api.themoviedb.org/3/search/tv?query=${req.query.search}`, {
+  const result = await axios.get(
+    `https://api.themoviedb.org/3/search/tv?query=${req.query.search}`,
+    {
       headers: {
         Authorization: `Bearer ${process.env.THEMOVIEDB_TOKEN}`,
       },
-    })
-    .then((result) => {
-      console.log("result", result.data.results);
-      res.send(result.data.results);
-    });
+    }
+  );
+
+  const response = result.data.results.map((val) => {
+    return {
+      poster_path: val.poster_path
+        ? `http://image.tmdb.org/t/p/w342/${val.poster_path}`
+        : url.format({
+            protocol: req.protocol,
+            host: req.get("host"),
+            pathname: "/placeholderposter",
+          }),
+      name: val.name,
+      year: val.first_air_date ? val.first_air_date.split("-")[0] : "Unknown",
+      id: val.id,
+    };
+  });
+
+  console.log("result", response);
+  return await res.send(response);
 });
 
 app.listen(port);
