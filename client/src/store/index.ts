fdb438d58bfaa8f99abb92f5a538f91ebda1e8b3
ZachDaChampion <zachchampion79@gmail.com>
2020-05-31 17:27:27 -0400

    Add search term to Vuex store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -5,14 +5,21 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    searchTerm: "",
     searchResults: Array<any>(),
   },
   mutations: {
+    updateSearchTerm(state, newTerm: string) {
+      state.searchTerm = newTerm;
+    },
     updateSearchResults(state, newResults: Array<any>) {
       state.searchResults = newResults;
     },
   },
   actions: {
+    updateSearchTerm(context, newTerm: string) {
+      context.commit("updateSearchTerm", newTerm);
+    },
     updateSearchResults(context, newResults: Array<any>) {
       context.commit("updateSearchResults", newResults);
     },
